535f89e180a9e2a8df19ebec75f0175ed6292932
kenny <kennyc680@gmail.com>
2020-08-13 13:23:06 -0600

    Initial Commit

---
+++ MyCocktail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCocktail
+//
+//  Created by Kenny Cardenas Reyes on 8/13/20.
+//  Copyright © 2020 Kenny Cardenas Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
