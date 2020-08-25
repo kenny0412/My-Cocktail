34f33d668e0945f76a6306fc61088d7bdbd19a60
kenny <kennyc680@gmail.com>
2020-08-25 10:49:25 -0600

    Map view

---
+++ MyCocktail/ViewControllers/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  MyCocktail
+//
+//  Created by Kenny Cardenas Reyes on 8/16/20.
+//  Copyright © 2020 Kenny Cardenas Reyes. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func textFieldEditingChanged(_ sender: Any) {
+    }
+    
+    
+}
